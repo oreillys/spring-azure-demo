11557be12aacd0c063b275a39ab13c86552ca176
orly <erictdh@gmail.com>
2023-01-02 12:44:48 +0800

    intial commit

---
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -0,0 +1,22 @@
+package com.javatechie.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringAzureDemoApplication {
+
+
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats, Orly! Your app deployed successfully in Azure";	
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
